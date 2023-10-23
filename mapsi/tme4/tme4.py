0373e9dec42c4181889ef19826a07bbb67d145dc
Simon Liétar <simon@lietar.net>
2023-10-23 14:20:28 +0200

    mapsi: Improve TME4

--- mapsi/tme4/tme4.py
+++ mapsi/tme4/tme4.py
@@ -78,21 +78,25 @@ def bad_init_4(x: np.ndarray):
   )
 
 def init_B(x: np.ndarray):
-  return np.array([0.1] * 10), x[0:30, :].reshape(3, 10, 256).sum(axis=0)
+  return np.array([0.1] * 10), x[0:30, :].reshape(10, 3, 256).mean(axis=1)
 
 def logpobsBernoulli(x: np.ndarray, theta: np.ndarray):
-  epsilon = 1e-8
+  epsilon = 1e-5
   bounded_theta = theta.clip(epsilon, 1.0 - epsilon)
   return (x * np.log(bounded_theta) + (1.0 - x) * np.log(1.0 - bounded_theta)).sum(axis=-1)
 
 def Q_i_B(x: np.ndarray, pi: np.ndarray, theta: np.ndarray):
-  result = np.zeros((x.shape[0], theta.shape[0]))
+  result = np.zeros((theta.shape[0], x.shape[0]))
 
-  for i in range(x.shape[0]):
-    a = np.argmax(logpobsBernoulli(x[i, :], theta))
-    result[i, a] = 1.0
+  for image_index in range(x.shape[0]):
+    a = logpobsBernoulli(x[image_index, :], theta) + np.log(pi)
+    s = np.max(a)
+    b = a - s - np.log(np.exp(a - s).sum())
 
-  return result.T
+    digit_index = np.argmax(b)
+    result[digit_index, image_index] = 1.0
+
+  return result
 
 def update_param_B(x: np.ndarray, q: np.ndarray, pi: np.ndarray, theta: np.ndarray):
   pi_u = q.sum(axis=1) / q.sum()
@@ -112,3 +116,30 @@ def EM_B(x: np.ndarray):
       break
 
   return nIter, pi, theta
+
+def calcul_purete(x: np.ndarray, y: np.ndarray, pi: np.ndarray, theta: np.ndarray):
+  result = Q_i_B(x, pi, theta).astype(int)
+
+  # Find the class of each cluster
+
+  clusters_y = np.zeros(result.shape[0], dtype=int)
+
+  for cluster_index in range(result.shape[0]):
+    a = y[np.where(result[cluster_index, :] == 1)]
+    b = np.bincount(a, minlength=result.shape[0])
+
+    clusters_y[cluster_index] = np.argmax(b)
+
+  # Compute the purity of each cluster
+
+  y_hat = clusters_y[np.argmax(result, axis=0)]
+  output = np.zeros((2, result.shape[0]))
+
+  for cluster_index, cluster_y in enumerate(clusters_y):
+    y_hat_c = y[y_hat == cluster_y]
+    count = np.bincount(y_hat_c, minlength=result.shape[0])
+
+    output[0, cluster_index] = count[cluster_y] / count.sum()
+    output[1, cluster_index] = count[cluster_y]
+
+  return (*output,)
