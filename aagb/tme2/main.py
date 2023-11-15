2f04b2fe398f57193b938c1c2a8dae315a4f2e81
agrislain <98108850+agrislain@users.noreply.github.com>
2023-11-15 22:40:25 +0100

    Add files via upload

--- aagb/tme2/main.py
+++ aagb/tme2/main.py
@@ -41,7 +41,7 @@ def is_additive(matrix: np.ndarray):
 # print(is_additive(M4))
 
 
-def is_multrametric(matrix: np.ndarray):
+def is_ultrametric(matrix: np.ndarray):
   for a in range(len(matrix)):
     for b in range(a):
       for c in range(b):
@@ -50,13 +50,64 @@ def is_multrametric(matrix: np.ndarray):
 
   return True
 
-# print(is_multrametric(M1))
-# print(is_multrametric(M2))
-# print(is_multrametric(M3))
-# print(is_multrametric(M4))
+# print(is_ultrametric(M1))
+# print(is_ultrametric(M2))
+# print(is_ultrametric(M3))
+# print(is_ultrametric(M4))
+
 
 
 def upgma(matrix: np.ndarray):
-  print(np.stack(matrix, axis=-1))
+  n = len(matrix)
+  clusters = [[i] for i in range(n)]
+  dist =[[matrix[i, j] for j in range(n)] for i in range(n)]
+  newick=""
 
-print(upgma(M1))
+  while n > 1:
+    # Find the minimum distance
+    min_dist = np.inf
+    for i in range(n):
+      for j in range(i):
+        if dist[i][j] < min_dist:
+          min_dist = dist[i][j]
+          min_j = i
+          min_i = j
+
+    # Create the new cluster
+    new_cluster = clusters[min_i] + clusters[min_j]
+    last_cluster_i= clusters[min_i]
+    last_cluster_j= clusters[min_j]
+    newick= "("+str(newick)+"("+str(last_cluster_i)+":"+str(min_dist)+","+str(last_cluster_j)+":"+str(min_dist)+")"+")"
+    # Update the clusters
+    clusters.pop(max(min_i, min_j))
+    clusters.pop(min(min_i, min_j))
+    clusters.append(new_cluster)
+  
+
+    # Update the distance matrix
+    new_dist = dist
+    for i in range(n):
+      if i!=min_i and i!=min_j:
+       new_dist[i].append((dist[min_i][i] * len(last_cluster_i) + dist[min_j][i] * len(last_cluster_j)) / (len(last_cluster_i) + len(last_cluster_j)))
+    new_dist.append([])
+    for i in range(n):
+      if i!=min_i and i!=min_j:
+       new_dist[n].append((dist[min_i][i] * len(last_cluster_i) + dist[min_j][i] * len(last_cluster_j)) / (len(last_cluster_i) + len(last_cluster_j)))
+    new_dist[n].append(0)
+    new_dist.pop(max(min_i, min_j))
+    new_dist.pop(min(min_i, min_j))
+    for i in range(n-2):
+      del new_dist[i][max(min_i, min_j)]
+      del new_dist[i][min(min_i, min_j)]
+    dist = new_dist
+    n -= 1
+
+
+  return newick
+
+mtest=np.array([[0,17,21,31,23],[17,0,30,34,21],[21,30,0,28,39],[31,34,28,0,43],[23,21,39,43,0]])
+print(upgma(M3))
+
+
+
+
