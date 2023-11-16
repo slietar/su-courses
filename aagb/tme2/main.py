4d400b57da6642323f9c713a2b9f8650eb938c6c
agrislain <98108850+agrislain@users.noreply.github.com>
2023-11-16 04:56:51 +0100

    Add files via upload

--- aagb/tme2/main.py
+++ aagb/tme2/main.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-from typing import Self
 import numpy as np
 
 
@@ -43,7 +41,7 @@ def is_additive(matrix: np.ndarray):
 # print(is_additive(M4))
 
 
-def is_multrametric(matrix: np.ndarray):
+def is_ultrametric(matrix: np.ndarray):
   for a in range(len(matrix)):
     for b in range(a):
       for c in range(b):
@@ -52,93 +50,63 @@ def is_multrametric(matrix: np.ndarray):
 
   return True
 
-# print(is_multrametric(M1))
-# print(is_multrametric(M2))
-# print(is_multrametric(M3))
-# print(is_multrametric(M4))
+# print(is_ultrametric(M1))
+# print(is_ultrametric(M2))
+# print(is_ultrametric(M3))
+# print(is_ultrametric(M4))
 
 
-@dataclass
-class Leaf:
-  value: int
+def upgma(matrix: np.ndarray):
+  n = len(matrix)
+  clusters = [[i] for i in range(n)]
+  dist =[[matrix[i, j] for j in range(n)] for i in range(n)]
+  newick=""
 
-@dataclass
-class Node:
-  children: list[tuple[Self | Leaf, float]]
+  while n > 1:
+    # Find the minimum distance
+    min_dist = np.inf
+    for i in range(n):
+      for j in range(i):
+        if dist[i][j] < min_dist:
+          min_dist = dist[i][j]
+          min_i = i
+          min_j = j
 
-  def _newick(self) -> str:
-    return '(' + ','.join([(chr(ord('A') + child.value) if isinstance(child, Leaf) else child._newick()) + f':{dist:.2f}' for child, dist in self.children]) + ')'
+    # Create the new cluster
+    new_cluster = clusters[min_i] + clusters[min_j]
+    last_cluster_i= clusters[min_i]
+    last_cluster_j= clusters[min_j]
+    newick= str(newick)+"("+str(last_cluster_i)+":"+str(min_dist)+","+str(last_cluster_j)+":"+str(min_dist)+")"
+    # Update the clusters
+    clusters.pop(max(min_i, min_j))
+    clusters.pop(min(min_i, min_j))
+    clusters.append(new_cluster)
+  
 
-  def newick(self):
-    return self._newick() + ';'
-
-# def add_nodes(a: Node | Leaf, b: Node | Leaf, da: float, db: float):
-#   match a, b:
-#     case (Node(), Node()) | (Leaf(), Leaf()):
-#       return Node([
-#         (a, da),
-#         (b, db)
-#       ])
-#     case Node(a_children), Leaf():
-#       return Node([
-#         *a_children,
-#         (b, da + db)
-#       ])
-#     case Leaf(), Node(b_children):
-#       return Node([
-#         (a, da + db),
-#         *b_children
-#       ])
-#     case _, _:
-#       raise RuntimeError
-
-
-def nj(matrix: np.ndarray):
-  m = matrix.astype(np.float64)
-  clusters: list[Node | Leaf] = [Leaf(i) for i in range(len(m))]
-
-  while (n := len(m)) > 2:
-    q: np.ndarray = (n - 2) * m - m.sum(axis=0) - m.sum(axis=1, keepdims=True)
-    q[np.diag_indices_from(q)] = np.inf
-
-    a, b = np.unravel_index(q.argmin(), q.shape)
-    a = int(a)
-    b = int(b)
-
-    da = 0.5 * m[a, b] + 0.5 / (n - 2) * (m[a, :].sum() - m[b, :].sum())
-    db = m[a, b] - da
-
-    clusters.append(Node([
-      (clusters[a], da),
-      (clusters[b], db)
-    ]))
-
-    del clusters[max(a, b)]
-    del clusters[min(a, b)]
-
-    new_row = 0.5 * (m[a, :] + m[b, :] - m[a, b])
-
-    indices = [i for i in range(n) if i != a and i != b]
-    m = m[indices, :][:, indices]
-    m = np.pad(m, (0, 1))
-
-    m[0:-1, -1] = new_row[indices]
-    m[-1, 0:-1] = new_row[indices]
-
-  root = Node([
-    (clusters[0], m[0, 1] * 0.5),
-    (clusters[1], m[0, 1] * 0.5)
-  ])
-
-  return root.newick()
+    # Update the distance matrix
+    new_dist = dist
+    for i in range(n):
+      if i!=min_i and i!=min_j:
+       new_dist[i].append((dist[min_i][i] * len(last_cluster_i) + dist[min_j][i] * len(last_cluster_j)) / (len(last_cluster_i) + len(last_cluster_j)))
+    new_dist.append([])
+    for i in range(n):
+      if i!=min_i and i!=min_j:
+       new_dist[n].append((dist[min_i][i] * len(last_cluster_i) + dist[min_j][i] * len(last_cluster_j)) / (len(last_cluster_i) + len(last_cluster_j)))
+    new_dist[n].append(0)
+    new_dist.pop(max(min_i, min_j))
+    new_dist.pop(min(min_i, min_j))
+    for i in range(n-2):
+      del new_dist[i][max(min_i, min_j)]
+      del new_dist[i][min(min_i, min_j)]
+    dist = new_dist
+    n -= 1
 
 
-x = np.array([
-  [0, 5, 9, 9, 8],
-  [5, 0, 10, 10, 9],
-  [9, 10, 0, 8, 7],
-  [9, 10, 8, 0, 3],
-  [8, 9, 7, 3, 0]
-], dtype=np.float64)
+  return newick
 
-print(nj(x))
+mtest=np.array([[0,17,21,31,23],[17,0,30,34,21],[21,30,0,28,39],[31,34,28,0,43],[23,21,39,43,0]])
+print(upgma(mtest))
+
+
+
+
