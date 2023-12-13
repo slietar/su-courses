a06519b6bcc6f8cf5c3ce69edb3cec9bd68b322a
Simon Liétar <simon@lietar.net>
2023-12-13 01:01:20 +0100

    aagb: Move TME5

--- aagb/tme5/main.py
+++ aagb/tme5/main.py
@@ -1,19 +1,24 @@
+from typing import IO
 import numpy as np
 from matplotlib import pyplot as plt
 from dataclasses import dataclass
 from pathlib import Path
-from scipy.optimize import curve_fit
+# from scipy.optimize import curve_fit
 
 
-@dataclass
+@dataclass(frozen=True, slots=True)
 class Graph:
   matrix: np.ndarray
+
+  def clustering_coefficient(self, node: int, /):
+    degrees = self.degrees()
+    print(degrees)
 
   def degrees(self):
     return self.matrix.sum(axis=0)
 
   @classmethod
-  def parse(cls, file):
+  def parse(cls, file: IO[str], /):
     edges = set[tuple[int, int]]()
     vertex_names = list[str]()
     vertex_count = 0
@@ -21,7 +26,7 @@ class Graph:
     def get_vertex(name: str):
       if name in vertex_names:
         return vertex_names.index(name)
-      
+
       vertex = len(vertex_names)
       vertex_names.append(name)
       return vertex
@@ -33,35 +38,29 @@ class Graph:
         a, b = [get_vertex(x) for x in line.split(',')]
         edges.add((a, b))
         vertex_count = max(vertex_count, a + 1, b + 1)
-    
+
     matrix = np.zeros((vertex_count, vertex_count), dtype=bool)
 
     for a, b in edges:
       matrix[a, b] = True
       matrix[b, a] = True
-    
+
     return Graph(matrix)
-    
 
-for name in ['reseau1', 'reseau2', 'reseau3']:
-  with Path(f'{name}.txt').open() as file:
-    graph = Graph.parse(file)
 
-  fig, ax = plt.subplots()
+# for name in ['reseau1', 'reseau2', 'reseau3']:
+#   with Path(f'{name}.txt').open() as file:
+#     graph = Graph.parse(file)
 
-  degrees = graph.degrees()
+#   degrees = graph.degrees()
 
-  power = lambda x, gamma: x ** -gamma
-  hist, bin_edges = h = np.histogram(degrees)
-  hist = np.maximum(hist, 1)
+#   fig, ax = plt.subplots()
 
-  bin_centers = (bin_edges[:-1] + bin_edges[1:]) * 0.5
-  gamma, pcov = curve_fit(power, bin_centers, hist) #, bounds=[1e-4, 10000])
-  print(gamma, pcov)
+#   ax.hist(degrees, bins=(degrees.max() - degrees.min()))
+#   fig.savefig(f'out_{name}.png')
 
-  ax.hist(degrees, bins=(degrees.max() - degrees.min()))
-  # ax.set_yscale('log')
 
-  s = np.array([0, bin_centers[-1]])
-  ax.plot(s, power(s, gamma))
-  fig.savefig(f'out_{name}.png')
+with Path('reseau3.txt').open() as file:
+  graph = Graph.parse(file)
+
+print(graph.clustering_coefficient(0))
