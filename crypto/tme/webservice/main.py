8a1c08dd3f65f4e00bdfb3186e027a5f92217393
Simon Liétar <simon@lietar.net>
2024-02-15 16:29:14 +0100

    crypto: Improve TME

--- crypto/tme/webservice/main.py
+++ crypto/tme/webservice/main.py
@@ -1,37 +1,94 @@
 import json
 from pathlib import Path
 from types import SimpleNamespace
-from OpenSSL import crypto
+
+import cryptography
+from cryptography import x509
+from cryptography.hazmat import primitives
+from cryptography.x509.oid import ExtensionOID, NameOID
 
 
-def load_file(path: str):
+def load_file(path: Path | str):
   with Path(path).open() as file:
     return json.load(file, object_hook=lambda x: SimpleNamespace(**x))
 
 
+# 0186-8263-0596-4099
 def verify(transaction):
-  cert = crypto.load_certificate(crypto.FILETYPE_PEM, transaction.card.certificate)
-  bank_cert = crypto.load_certificate(crypto.FILETYPE_PEM, transaction.card.bank.certificate)
+  cert = x509.load_pem_x509_certificate(transaction.card.certificate.encode())
+  bank_cert = x509.load_pem_x509_certificate(transaction.card.bank.certificate.encode())
 
-  try:
-    crypto.verify(cert, bytes.fromhex(transaction.signature), transaction.data.encode(), 'sha256')
-  except crypto.Error:
+  data = json.loads(transaction.data)
+
+  get_name = lambda x: x.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+  if get_name(cert.subject) != transaction.card.number:
     return False
 
-  # bank_cert.set_issuer(bank_cert.get_subject())
+  if get_name(bank_cert.subject) != transaction.card.bank.name:
+    return False
 
-  print(bank_cert.get_issuer())
-  print(bank_cert.get_subject())
+  if get_name(bank_cert.subject) != data['bank-name']:
+    return False
 
-  store = crypto.X509Store()
-  store.add_cert(bank_cert)
-  context = crypto.X509StoreContext(store, bank_cert)
-  context.verify_certificate()
+  if get_name(cert.subject) != data['card-number']:
+    return False
 
-  # print(cert.get_issuer() == bank_cert.get_subject())
+  if get_name(bank_cert.issuer) != '__CA__':
+    return False
+
+  if transaction.card.number == '1132-7310-2482-6732':
+    print(transaction.card.bank.certificate)
+
+  try:
+    ext = bank_cert.extensions.get_extension_for_oid(ExtensionOID.BASIC_CONSTRAINTS)
+
+    if not ext.value.ca:
+      return False
+  except cryptography.x509.extensions.ExtensionNotFound:
+    return False
+
+  try:
+    cert.public_key().verify(
+      bytes.fromhex(transaction.signature),
+      transaction.data.encode(),
+      signature_algorithm=primitives.asymmetric.ec.ECDSA(primitives.hashes.SHA256())
+    )
+  except cryptography.exceptions.InvalidSignature:
+    return False
+
+  try:
+    cert.verify_directly_issued_by(bank_cert)
+  except (ValueError, cryptography.exceptions.InvalidSignature):
+    return False
+
+  # try:
+  #   bank_cert.public_key().verify(
+  #     bank_cert.signature,
+  #     bank_cert.tbs_certificate_bytes
+  #   )
+  # except cryptography.exceptions.InvalidSignature:
+  #   return False
+
+  # store = x509.verification.Store([bank_cert])
+  # builder = x509.verification.PolicyBuilder() #.add_default_policy(x509.verification.BasicConstraints()).build(store)
+  # verifier = builder.build_server_verifier()
 
   return True
 
 
-print(verify(load_file('sample_invalid.json').transaction))
-# print(verify(load_file('sample_valid.json').transaction))
+# assert verify(load_file('samples/valid.json').transaction)
+
+# for path in Path('samples/invalid').glob('*.json'):
+#   print(path)
+#   assert not verify(load_file(path).transaction)
+#   print()
+#   print()
+
+
+batch = load_file('batch.json').batch
+
+print(batch.identifier)
+print(','.join(['1' if verify(transaction) else '0' for transaction in batch.transactions]))
+
+print(batch)
