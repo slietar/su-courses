a2aefee8c270f9e014089f4a96c9b9f2a9613039
Simon Liétar <simon@lietar.net>
2024-03-26 19:14:07 +0100

    project: Improve GEMME, CV and SASA plots

--- project/analysis/plots/cv_sasa.py
+++ project/analysis/plots/cv_sasa.py
@@ -1,16 +1,21 @@
-from matplotlib import pyplot as plt
 import pandas as pd
-from .. import shared
+from matplotlib import pyplot as plt
+
+from .. import data, shared
 from ..cv import cv
 from ..sasa import sasa
 
 
-df = pd.concat([cv, sasa.total.rename('sasa')], axis='columns')
+pathogenic = data.all_mutations.groupby('position').pathogenicity.aggregate(lambda x: ((1 <= x) & (x <= 3)).any()).rename('pathogenic')
+
+df = pd.concat([cv, sasa.total.rename('sasa'), pathogenic], axis='columns')
+df.pathogenic.fillna(False, inplace=True)
+
 
 fig, ax = plt.subplots()
 
-ax.scatter(df.cv, df.sasa, s=4)
-ax.set_xlabel('Circular Variance')
+ax.scatter(df.cv, df.sasa, c=df.pathogenic, s=4)
+ax.set_xlabel('Circular variance')
 ax.set_ylabel('Relative total SASA')
 
 with (shared.output_path / 'cv_sasa.png').open('wb') as file:
--- project/analysis/plots/gemme_cv.py
+++ project/analysis/plots/gemme_cv.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 
 from .. import data, gemme, shared
-from ..sasa import sasa
+from ..cv import cv
 
 
 a = data.mutations.apply(lambda mutation: gemme.gemme.loc[mutation.position, mutation.alternate_aa], axis='columns') # type: ignore
@@ -13,15 +13,15 @@ fig, ax = plt.subplots(figsize=(10, 8))
 ax.axline((0, data.gemme_threshold), color='gray', linestyle='--', slope=0)
 ax.axline((data.gemme_threshold, 0), (data.gemme_threshold, 1), color='gray', linestyle='--')
 
-scatter = ax.scatter(b, a, c=sasa.total.loc[data.mutations.position], cmap='RdYlGn', s=5.5)
+scatter = ax.scatter(b, a, c=cv.loc[data.mutations.position], cmap='RdYlGn', s=5.5)
 
 ax.set_xlabel('Mean')
 ax.set_ylabel('Mutation')
 
 cbar = fig.colorbar(scatter, ax=ax)
 cbar.ax.get_yaxis().labelpad = 15
-cbar.ax.set_ylabel('Relative total SASA', rotation=270)
+cbar.ax.set_ylabel('Circular variance', rotation=270)
 
 
-with (shared.output_path / 'gemme_sasa.png').open('wb') as file:
+with (shared.output_path / 'gemme_cv.png').open('wb') as file:
   plt.savefig(file, dpi=300)
