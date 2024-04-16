66707f770cf4372fbc9612ed77f2959f06086ab4
Simon Liétar <simon@lietar.net>
2024-04-16 14:42:50 +0200

    project: Improve MSA analysis

--- project/analysis/msa.py
+++ project/analysis/msa.py
@@ -1,42 +1,47 @@
 import numpy as np
 import pandas as pd
 
-from . import data, shared
+from . import data, shared, utils
 
 
 # for domain_index, (_, domain) in enumerate(data.domains.iterrows()):
-#   if domain.kind == 'EGFCB':
+#   if domain.kind == 'TB':
 #     print(f'> {domain_index:04}')
 #     print(data.sequence[(domain.start_position - 1):domain.end_position])
 
 
-with (shared.root_path / 'sources/alignments/EGFCB.txt').open() as file:
-  file.readline()
-  lines = [line.split() for line in file.read().splitlines() if line and not line.startswith(' ')]
+def process_domain_kind(domain_kind: str):
+  with (shared.root_path / f'sources/alignments/{domain_kind}.txt').open() as file:
+    file.readline()
+    lines = [line.split() for line in file.read().splitlines() if line and not line.startswith(' ')]
 
-  offsets = [1] * len(lines)
-  length = len(lines[0][1])
+    offsets = [1] * len(lines)
+    length = len(lines[0][1])
 
-  columns = np.zeros((length, len(lines)), dtype=int)
+    columns = np.zeros((length, len(lines)), dtype=int)
 
-  for position_index in range(length):
-    for line_index, line in enumerate(lines):
-      if line[1][position_index] != '-':
-        columns[position_index, line_index] = offsets[line_index]
-        offsets[line_index] += 1
+    for position_index in range(length):
+      for line_index, line in enumerate(lines):
+        if line[1][position_index] != '-':
+          columns[position_index, line_index] = offsets[line_index]
+          offsets[line_index] += 1
 
-  msa_ = pd.DataFrame(columns.T, columns=range(1, length + 1), index=[data.domains.iloc[int(line[0])].name for line in lines])
-  msa_.sort_index(inplace=True, key=(lambda x: data.domains.loc[x].number))
+    msa = pd.DataFrame(columns.T, columns=range(1, length + 1), index=[data.domains.iloc[int(line[0])].name for line in lines])
+    msa.sort_index(inplace=True, key=(lambda x: data.domains.loc[x].number))
 
-  msa = {
-    'EGFCB': msa_
-  }
+  return msa
 
+@utils.cache
+def compute_msa():
+  return { domain_kind: process_domain_kind(domain_kind) for domain_kind in data.domain_kinds }
 
-if __name__ == '__main__':
-  print(msa)
+msa = compute_msa()
 
 
 __all__ = [
   'msa'
 ]
+
+
+if __name__ == '__main__':
+  print(msa)
---
+++ project/analysis/utils.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import pickle
+import shutil
+from tempfile import NamedTemporaryFile
+from typing import Callable, ParamSpec, TypeVar
+
+
+cache_path = Path.home() / '.cache' / 'su-project'
+cache_path.mkdir(exist_ok=True, parents=True)
+
+
+P = ParamSpec('P')
+T = TypeVar('T')
+
+def cache(fn: Callable[P, T], /):
+  def new_fn(*args: P.args, **kwargs: P.kwargs):
+    if fn.__module__ == '__main__':
+      return fn(*args, **kwargs)
+
+    name = f'{fn.__module__}:{fn.__qualname__}'
+    path = cache_path / name
+
+    if path.exists():
+      with path.open('rb') as file:
+        result: T = pickle.load(file)
+
+      return result
+    else:
+      result = fn(*args, **kwargs)
+
+      with NamedTemporaryFile('wb', delete=False) as file:
+        pickle.dump(result, file)
+
+      Path(file.name).replace(path)
+
+      return result
+
+  return new_fn
+
+
+if __name__ == '__main__':
+  print('Clearing cache')
+  shutil.rmtree(cache_path)
