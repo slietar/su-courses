7a86faab995311e5803408c6818d0c6cb8c9f034
Simon Liétar <simon@lietar.net>
2024-05-20 00:58:01 +0200

    project: Add GEMME with orthologs

--- project/analysis/gemme.py
+++ project/analysis/gemme.py
@@ -1,41 +1,65 @@
 import csv
+from dataclasses import dataclass
+import functools
+from typing import IO
 
 import numpy as np
 import pandas as pd
 
-from . import data, shared
+from . import data, shared, utils
 
 
-def conv(x: bytes):
-  return x if x != b'NA' else np.nan
+@dataclass
+class GEMMEData:
+  array: np.ndarray
+  dataframe: pd.DataFrame
 
-threshold = -0.779
+  @functools.cached_property
+  def mean(self):
+    return self.dataframe.mean(axis='columns').rename('gemme_mean')
 
-with (shared.root_path / 'drive/P35555/P35555_normPred_evolCombi.txt').open() as file:
+def load_gemme(file: IO[str], /):
   file.readline()
   reader = csv.reader(file, delimiter=' ')
 
   items = [[cell if cell != 'NA' else 'nan' for cell in row] for row in reader]
+  full_arr = np.array(items)
+  aas = [res.upper() for res in full_arr[:, 0]]
 
-full_arr = np.array(items)
-aas = [res.upper() for res in full_arr[:, 0]]
+  arr = full_arr[:, 1:].astype(float)
+  dataframe = pd.DataFrame(arr.T, columns=aas, index=data.position_index)
 
-gemme_arr = full_arr[:, 1:].astype(float)
+  return GEMMEData(arr, dataframe)
 
-gemme = pd.DataFrame(gemme_arr.T, columns=aas, index=pd.Series(np.arange(data.protein_length) + 1, name='position'))
-gemme_mean = gemme.mean(axis='columns').rename('gemme_mean')
+
+@utils.cache()
+def compute_gemme():
+  with (shared.root_path / 'drive/P35555/P35555_normPred_evolCombi.txt').open() as file:
+    gemme_all = load_gemme(file)
+
+  with (shared.root_path / 'sources/gemme_orthologs.txt').open() as file:
+    gemme_orthologs = load_gemme(file)
+
+  return gemme_all, gemme_orthologs
+
+
+gemme_all, gemme_orthologs = compute_gemme()
+
+# Deprecated
+gemme = gemme_all.dataframe
+gemme_arr = gemme_all.array
+gemme_mean = gemme_all.mean
 
 
 __all__ = [
-  'gemme',
-  'gemme_arr',
-  'gemme_mean'
+  'gemme_all',
+  'gemme_orthologs'
 ]
 
 
 if __name__ == '__main__':
-  print('GEMME')
-  print(gemme)
+  print('GEMME all')
+  print(gemme_all.dataframe.join(gemme_all.mean))
 
-  print('\n\nGEMME mean')
-  print(gemme_mean)
+  print('\n\nGEMME orthologs')
+  print(gemme_orthologs.dataframe.join(gemme_orthologs.mean))
